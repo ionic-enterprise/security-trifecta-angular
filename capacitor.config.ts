934eb9bef6c0d6ff9acb988d545b04798814d4ac
damian.ionic <damian@ionic.io>
2023-11-07 07:36:44 -0800

    fix: use initialize to setup vault, update to Angular 16

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.conferences.eas2021',
   appName: 'EAS 21',
   webDir: 'www',
-  bundledWebRuntime: false,
   // cordova: {
   //   preferences: {
   //     "DisableDeploy": "true"
