b3ce22c72ac10665acab48db6085c57f0ac56408
damian.ionic <damian@ionic.io>
2024-02-26 08:44:01 -0800

    feat: add launch from home

---
+++ android/app/src/main/java/io/ionic/cs/appsummit/LauncherActivity.java
@@ -0,0 +1,13 @@
+package io.ionic.cs.appsummit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
