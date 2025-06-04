ef30d1eb51c169d0ee1220ca9321a3e1db0bc749
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-06-04 12:46:35 -0500

    update identity vault, use privacy screen plugin

--- src/app/services/vault.service.ts
+++ src/app/services/vault.service.ts
@@ -1,92 +1,99 @@
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import {
-    BiometricSecurityStrength,
-    BrowserVault, Device, DeviceSecurityType,
-    IdentityVaultConfig, Vault, VaultError, VaultErrorCodes, VaultType
+  BiometricSecurityStrength,
+  BrowserVault,
+  Device,
+  DeviceSecurityType,
+  IdentityVaultConfig,
+  Vault,
+  VaultError,
+  VaultErrorCodes,
+  VaultType,
 } from '@ionic-enterprise/identity-vault';
 import { RouteService } from './route.service';
 import { AuthResult } from '@ionic-enterprise/auth';
+import { PrivacyScreen } from '@capacitor/privacy-screen';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class VaultService {
-    config: IdentityVaultConfig = {
-        key: 'io.ionic.conferences.cs.auth',
-        type: VaultType.DeviceSecurity,
-        deviceSecurityType: DeviceSecurityType.Biometrics,
-        lockAfterBackgrounded: 2000,
-        shouldClearVaultAfterTooManyFailedAttempts: false,
-        customPasscodeInvalidUnlockAttempts: 10,
-        unlockVaultOnLoad: false,
-    };
+  config: IdentityVaultConfig = {
+    key: 'io.ionic.conferences.cs.auth',
+    type: VaultType.DeviceSecurity,
+    deviceSecurityType: DeviceSecurityType.Biometrics,
+    lockAfterBackgrounded: 2000,
+    shouldClearVaultAfterTooManyFailedAttempts: false,
+    customPasscodeInvalidUnlockAttempts: 10,
+    unlockVaultOnLoad: false,
+  };
 
-    vault: Vault | BrowserVault;
+  vault: Vault | BrowserVault;
 
-    constructor(private routeService: RouteService) {
+  constructor(private routeService: RouteService) {}
+
+  /**
+   * Init is called by our APP_INITIALIZER at the startup of the application
+   */
+  public async init() {
+    if (Capacitor.getPlatform() === 'web') {
+      this.vault = new BrowserVault();
+    } else {
+      // If the device doesnt have biometrics the we'll use a Secure Storage Vault
+      if (!(await this.hasBiometrics())) {
+        this.config = {
+          ...this.config,
+          key: 'io.ionic.conferences.cs.auth.alternate',
+          type: VaultType.SecureStorage,
+          deviceSecurityType: DeviceSecurityType.None,
+        };
+      }
+      this.vault = new Vault();
     }
+    await this.vault.initialize(this.config);
 
-    /**
-     * Init is called by our APP_INITIALIZER at the startup of the application
-     */
-    public async init() {
-        if (Capacitor.getPlatform() === 'web') {
-            this.vault = new BrowserVault();
-        } else {
-            // If the device doesnt have biometrics the we'll use a Secure Storage Vault
-            if (!await this.hasBiometrics()) {
-                this.config = {
-                    ...this.config,
-                    key: 'io.ionic.conferences.cs.auth.alternate',
-                    type: VaultType.SecureStorage,
-                    deviceSecurityType: DeviceSecurityType.None
-                };
-            }
-            this.vault = new Vault();
-        }
-        await this.vault.initialize(this.config);
+    this.vault.onConfigChanged(() => {
+      console.log('Vault configuration was changed', this.config);
+    });
 
-        this.vault.onConfigChanged(() => {
-            console.log('Vault configuration was changed', this.config);
-        });
+    this.vault.onLock(() => {
+      console.log('Vault was locked');
+    });
 
-        this.vault.onLock(() => {
-            console.log('Vault was locked');
-        });
+    this.vault.onUnlock(() => {
+      console.log('Vault was unlocked');
+    });
 
-        this.vault.onUnlock(() => {
-            console.log('Vault was unlocked');
-        });
+    this.vault.onError((error: VaultError) => {
+      console.error('this.vault.onError', error);
+      if (error.code === VaultErrorCodes.UserCanceledInteraction) {
+        this.routeService.returnToLogin();
+      }
+    });
 
-        this.vault.onError((error: VaultError) => {
-            console.error('this.vault.onError', error);
-            if (error.code === VaultErrorCodes.UserCanceledInteraction) {
-                this.routeService.returnToLogin();
-            }
-        });
+    await PrivacyScreen.enable({ android: { dimBackground: true, privacyModeOnActivityHidden: 'splash' } });
+  }
 
-        // If you would like the privacy screen set to true
-        await Device.setHideScreenOnBackground(true);
-    }
+  public async clear() {
+    await this.vault.clear();
+  }
 
-    public async clear() {
-        await this.vault.clear();
-    }
+  public async set(value: AuthResult) {
+    await this.vault.setValue('auth', JSON.stringify(value));
+  }
 
-    public async set(value: AuthResult) {
-        await this.vault.setValue('auth', JSON.stringify(value));
-    }
+  public async get(): Promise<AuthResult | undefined> {
+    const value = await this.vault.getValue('auth');
+    if (value == null) return undefined;
+    return JSON.parse(value);
+  }
 
-    public async get(): Promise<AuthResult | undefined> {
-        const value = await this.vault.getValue('auth');
-        if (value == null) return undefined;
-        return JSON.parse(value);
-    }
-
-    private async hasBiometrics(): Promise<boolean> {
-        // For this app we only want to use biometrics if the device is capable of strong encryption
-        return await Device.isBiometricsEnabled() &&
-            (await Device.getBiometricStrengthLevel() === BiometricSecurityStrength.Strong);
-    }
+  private async hasBiometrics(): Promise<boolean> {
+    // For this app we only want to use biometrics if the device is capable of strong encryption
+    return (
+      (await Device.isBiometricsEnabled()) &&
+      (await Device.getBiometricStrengthLevel()) === BiometricSecurityStrength.Strong
+    );
+  }
 }
